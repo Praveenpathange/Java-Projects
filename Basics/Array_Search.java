1c1d986e7f313e693246bdf1574e1f1a11e2b791
Praveenpathange <praveenpathange07@gmail.com>
2026-01-21 21:14:00 +0530

    Add files via upload

---
+++ Basics/Array_Search.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Array_Search{
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the size of array : ");
+       int size = sc.nextInt();
+       sc.nextLine();
+       String[] name = new String[size];
+       System.out.println("Enter the "+size+" Words : ");
+       
+       for(int i=0;i<size;i++){
+         name[i] = sc.nextLine();
+         name[i] = name[i].toLowerCase();
+       }
+
+       System.out.print("Enter the target : ");
+       String target = sc.nextLine();
+       target = target.toLowerCase();
+       int found=0;
+
+       for(int i=0;i<name.length;i++){
+        if(target.equals(name[i])){
+            found=1;  
+        }
+       }
+       if(found==1){
+        System.out.println(target+" is found");
+       }else{
+        System.out.println("Not found");
+       }
+        sc.close();
+    }
+}
